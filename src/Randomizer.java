60d4ed97929666af2c481f8fdc58718da7fc4a7e
dimitrisgkountelos <gkounted@csd.auth.gr>
2020-11-12 23:26:08 +0200

    Made randomize methon in class: Randomizer, using 3 for-loops

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -6,5 +6,32 @@ import java.util.Random;
  * @autho Gkountelos Dimitrios
  */
 public class Randomizer {
-    
+    /**
+     * this method randomizes numbers in range(start, end)
+     * @param start is the first number of the range
+     *              that will get randomized
+     * @param end is the last number
+     * @return the array with the numbers start->end shuffled
+     */
+    public int[] randomize(int start, int end) {
+        int[] shuffled = new int[end+1-start];
+        Random random = new Random();
+        for (int i=0; i<shuffled.length; i++) {
+            shuffled[i] = i;
+        }
+
+        int temp, selected;
+        for (int i=shuffled.length-1; i>=0; i--) {
+            selected = random.nextInt(i);// random number between 0-i(current index)
+            // swap the current array value (i) with a randomly selected one
+            temp = shuffled[i];
+            shuffled[i] = shuffled[selected];
+            shuffled[selected] = temp;
+        }
+
+        for (int i=0; i<shuffled.length; i++) {
+            shuffled[i] = shuffled[i+start];
+        }
+        return shuffled;
+    }
 }
