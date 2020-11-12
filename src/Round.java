3217e4a8a219c7ab4f247dce9c4b19f751e5e3bf
dimitrisgkountelos <gkounted@csd.auth.gr>
2020-11-12 19:14:59 +0200

    fixed a minor bug

--- src/Round.java
+++ src/Round.java
@@ -12,15 +12,14 @@ public class Round {
     /**
      * Round takes as parameters, 2 arrays and the question category
      */
-    Scanner console = new Scanner(System.in);
     private int points;
     private CliInterface CLI;
-
+    
     public Round() {
         this.points = 0;
         this.CLI = new CliInterface();
     }
-
+    
     /**
      * this method is for the "right answer" type of round and it
      * gets an array: questions[20,5] (String) as input
@@ -41,13 +40,13 @@ public class Round {
         question[2] = "128 meters";
         question[3] = "256 meters";
         question[4] = "512 meters";
-       
+        
         if (CLI.interaction(question) == true) {
             //this.points += 1000;
         }
-
+        
     }
-
+    
     /**
      * this method is for "bidding" type of round and it
      * gets an array: questions[20,5] (String) as input
@@ -67,15 +66,18 @@ public class Round {
         question[2] = "128 meters";
         question[3] = "256 meters";
         question[4] = "512 meters";
-
+        
         System.out.println("The category is bidding!");
         System.out.println("You can bid 250,500,750 and 1000 points");
+        Scanner console = new Scanner(System.in);
         int bid_player = console.nextInt();    // here it takes an input "bid" from the user
         while (bid_player != 250 && bid_player != 500 && bid_player != 750 && bid_player != 1000){ //checks for wrong input
             System.out.println("You can't bid" + console + "points");
             System.out.println("Please bid again");
             bid_player = console.nextInt();
         }
+        console.close();
+        
         if (CLI.interaction(question) == true) {
             System.out.println("take your" + bid_player + "that you had bidden");
             //this.points += bid_player;
