146a77dad40cd27bfcb6246657d283e6c81b4c27
dimitrisgkountelos <gkounted@csd.auth.gr>
2020-11-14 20:24:16 +0200

    Overloaded constructor of class: Qac

--- src/Qac.java
+++ src/Qac.java
@@ -18,7 +18,20 @@ public class Qac {
         this.answers[2] = answers[2];
         this.answers[3] = answers[3];
     }
-
+    /**
+     * overloading of the constructor above
+     */
+    public Qac(String question, String answer1, 
+                String answer2, String answer3, 
+                String answer4, String category) {
+        this.question = question;
+        this.category = category;
+        this.answers = new String[4];
+        this.answers[0] = answer1;
+        this.answers[1] = answer2;
+        this.answers[2] = answer3;
+        this.answers[3] = answer4;
+    }
     /**
      * getter for the question field
      */
