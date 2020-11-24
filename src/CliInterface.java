8de93e43f57bb9e995da675cb62c1bde46d2bd7e
dimitrisgkountelos <gkounted@csd.auth.gr>
2020-11-24 03:34:19 +0200

    Fixed bug in mathod: interaction

--- src/CliInterface.java
+++ src/CliInterface.java
@@ -16,48 +16,32 @@ public class CliInterface {
      * @return true if user won or false if he lost
      */
     public boolean interaction(Qac qac) {
-        System.out.println(qac.getQuestion()); // that prints the question
-        
-        int correctAnswersPlace;
+        System.out.println(qac.getQuestion());
+        int correctAnswersPlace = 0;
         Randomizer randomizer = new Randomizer();
-        int[] questionNumbers = randomizer.randomize(1, 4);// shuffle numbers 1-4
-
-        // indexes are in correct order (0-3)
-        // so this holds the number that is shown to the user
-        // for the correct answer
-        correctAnswersPlace = questionNumbers[0];
-        
-        // this prints the answers in random order
-        System.out.println(" 1."+qac.getAnswers()[questionNumbers[0]-1]+
-                        " 2."+ qac.getAnswers()[questionNumbers[1]-1]);
-        
-        System.out.println(" 3."+qac.getAnswers()[questionNumbers[2]-1]+
-                        " 4."+ qac.getAnswers()[questionNumbers[3]-1]);
-
-        System.out.println("Select between: 1, 2, 3, 4 ");
-        System.out.println("Answer:");
-        Scanner chosen = new Scanner(System.in);
-        int playersAnswer = chosen.nextInt();
-        short counter=1;
-        while (playersAnswer!=1 && playersAnswer!=2 && 
-                playersAnswer!=3 && playersAnswer!=4) {
-            if (counter == 1) {
-                System.out.println("Wrong Input...please man its 1,2,3,4 how hard can it be??");
+        int[] questionNumbers = randomizer.randomize(0, 3);
+        // this holds the randomized number of the correct answer
+        // correctAnswersPlace = questionNumbers[0];
+        for (int i=0; i<4; i++) {
+            if (questionNumbers[i] == 0) {
+                correctAnswersPlace = i;
             }
-            else {
-                System.out.println("I SAID 1,2,3 or freaking 4, WTF is wrong with you");
-            }
-            playersAnswer = chosen.nextInt();
-            counter += 1;
         }
-        
-        if (playersAnswer == correctAnswersPlace) {
-            System.out.println("You Win");
+
+        System.out.println("1."+qac.getAnswers()[questionNumbers[0]]);
+        System.out.println("2."+qac.getAnswers()[questionNumbers[1]]);
+        System.out.println("3."+qac.getAnswers()[questionNumbers[2]]);
+        System.out.println("4."+qac.getAnswers()[questionNumbers[3]]);
+
+        Scanner scanner = new Scanner(System.in);
+        int playersAnswer = scanner.nextInt();
+
+        if ((playersAnswer - 1) == correctAnswersPlace) {
+            System.out.println("Correct");
             return true;
         }
         else {
-            System.out.println("You idiot, the correct answer is: "+
-                                correctAnswersPlace);
+            System.out.println("Nope");
             return false;
         }
     }
