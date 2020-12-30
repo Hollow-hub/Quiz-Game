48a443af4ece195fe912c512856bb0562d076759
dimitris gkountelos <dimitrisgkountelos1@gmail.com>
2020-12-30 14:23:41 +0200

    Made method play to read questions from file questions.txt and overloaded constructor of Qac to simplify things

--- src/Qac.java
+++ src/Qac.java
@@ -9,20 +9,20 @@ public class Qac {
     private final String[] answers;
     /**
      * the constructor of the class
-     * @param question is an object from the class Qac
-     * @param answers is an object from the class Qac
-     * @param category is an object from the class Qac
-     * all of them determine one object in the Arraylist qac
-     *
+     * @param all is a string array with the 
+     * question at the first position. 
+     * 1-3 are the possible answers with the
+     * correct at all[1] and all[5] is the
+     * category
      */
-    public Qac(String question, String[] answers, String category) {
-        this.question = question;
-        this.category = category;
+    public Qac(String[] all) {
+        this.question = all[0];
         this.answers = new String[4];
-        this.answers[0] = answers[0];
-        this.answers[1] = answers[1];
-        this.answers[2] = answers[2];
-        this.answers[3] = answers[3];
+        this.answers[0] = all[1];
+        this.answers[1] = all[2];
+        this.answers[2] = all[3];
+        this.answers[3] = all[4];
+        this.category = all[5];
     }
     /**
      * overloading of the constructor above
