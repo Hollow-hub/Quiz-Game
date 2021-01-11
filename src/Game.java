f57f2ccc0a0add363ca48807b0e3e72c44514945
Hollow-hub <stefanosks@csd.auth.gr>
2021-01-11 21:00:08 +0200

    images added

--- src/Game.java
+++ src/Game.java
@@ -86,7 +86,7 @@ public class Game{
         String typeOfRound;
         Round round = new Round();
         Random random = new Random();
-        int rounds = qac.size()/13;
+        int rounds = qac.size()/6;
         
         // a loop that runs each round 
         int shownRoundNumber = 1;
